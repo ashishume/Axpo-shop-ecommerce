518ca1cd87d97ae392d9a2d953aeed0d5fdb912a
Ashish Debnath <ashishume@gmail.com>
2023-07-21 00:06:31 +0530

    feat: frontend setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 4000;
 const userRoutes = require("./routes/Users");
 const productRoutes = require("./routes/Products");
 const categoryRoutes = require("./routes/Category");
