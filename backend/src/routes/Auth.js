f1895dfe5f1a077444ba84d98e1cc8dbd48f7c86
Ashish Debnath <ashishume@gmail.com>
2023-08-08 15:49:11 +0530

    feat: logout fixed

--- backend/src/routes/Auth.js
+++ backend/src/routes/Auth.js
@@ -51,7 +51,7 @@ router.get("/validate", async (req, res) => {
 });
 router.post("/logout", async (req, res) => {
   try {
-    res.clearCookie("jwt", { httpOnly: true });
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "none", secure: true });
     res.status(200).json({ message: "Logged out successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
