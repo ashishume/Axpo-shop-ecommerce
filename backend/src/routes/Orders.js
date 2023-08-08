c43fd40f1f8fb74b1c7a6ce917a27140e8dfdb02
Ashish Debnath <ashishume@gmail.com>
2023-08-08 23:58:17 +0530

    feat: orders deletion

--- backend/src/routes/Orders.js
+++ backend/src/routes/Orders.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const router = express.Router();
-const Orders = require("../models/Orders")
+const Orders = require("../models/Orders");
+const Cart = require("../models/Cart");
 const authenticateToken = require("../controllers/authMiddleware");
 
-router.post("/orders/create", async (req, res) => {
+router.post("/orders/create", authenticateToken, async (req, res) => {
   try {
     const { user, products } = req.body;
     const newOrders = new Orders({ user, products });
     await newOrders.save();
-    res.status(201).json({ message: "orders created" });
+    const productIds = products.map((value) => value.product);
+    if (productIds?.length) {
+      /** when order is placed successfully delete the cart data */
+      const cart = await Cart.deleteMany({ product: { $in: productIds }, user });
+      if (cart) res.status(201).json({ message: "Orders placed successfully" });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 });
 
-router.get("/orders/:userId", async (req, res) => {
+router.get("/orders/:userId", authenticateToken, async (req, res) => {
   try {
     const { userId } = req.params;
     const orders = await Orders.find({
