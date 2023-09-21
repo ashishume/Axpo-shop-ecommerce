a1a5970e353cd24c272d68f0835c813bc94482b0
Ashish Debnath <ashishume@gmail.com>
2023-09-22 01:09:06 +0530

    feat: train booking logic

--- backend/src/routes/Booking/TrainBooking.js
+++ backend/src/routes/Booking/TrainBooking.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const TrainBooking = require("../../models/Booking/trainBooking");
 const authenticateToken = require("../../controllers/authMiddleware");
-
+const stationData = require("../../constants/stations");
 router.post("/train/book", async (req, res) => {
   try {
     const { trainNo, stations, fromDate, toDate, time } = req.body;
@@ -25,10 +25,43 @@ router.post("/trains", async (req, res) => {
   try {
     const { source, destination } = req.body;
     const trains = await TrainBooking.find({
-      "stations.location": source,
-      "stations.location": destination,
+      "stations.railwayStation": source,
+      "stations.railwayStation": destination,
     });
+    let sourceIndex = null;
+    let destinationIndex = null;
+    if (trains) {
+      trains.forEach((train) => {
+        train.stations.forEach((station, index) => {
+          if (source === station.railwayStation) {
+            sourceIndex = index;
+          }
+          if (destination === station.railwayStation) {
+            destinationIndex = index;
+          }
+        });
+      });
+    }
+    if (sourceIndex < destinationIndex) {
+      const sourceData = stationData.stations.find((value) => value.railwayStation === source);
+      const destinationData = stationData.stations.find((value) => value.railwayStation === destination);
+      return res.status(201).json({
+        trains,
+        source: sourceData,
+        destination: destinationData,
+      });
+    } else {
+      return res.status(200).json([]);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
+/** get all trains */
+router.get("/trains", async (req, res) => {
+  try {
+    const trains = await TrainBooking.find();
     res.status(200).json(trains);
   } catch (error) {
     res.status(500).json({ message: error.message });
